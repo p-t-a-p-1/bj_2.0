555e5c922cad39810378cb4609dd99c2aa5e8f04
p-t-a-p-1 <kb240225+1@gmail.com>
2020-05-06 15:35:01 +0900

    [add] Cardクラスのファイル追加

---
+++ card.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+
+class Card:
+    """
+    カードのマークと数字を出力
+
+    Attributes
+    ----------
+    card_mark : int
+        カードのマーク（♠︎❤︎♦︎♣️）
+    card_value : int
+        カードの数字
+    """
+
+    # マーク
+    marks = ["♠︎-", "❤︎-", "♦︎-", "♣️-"]
+    # 数字
+    values = ([None, None, 2, 3, 4, 5, 6,
+               7, 8, 9, 10, 11, 12, 13, 1])
+
+    def __init__(self, card_mark, card_value):
+        self.mark = card_mark
+        self.value = card_value
+
+    # オーバーライドしてインスタンスの出力内容を変更
+    def __repr__(self):
+        return self.marks[self.mark] + str(self.values[self.value])
+
+
+# Cardクラスからインスタンスを生成（引数にマークと数字）
+# reprメソッドで出力内容を変更しているので下記のように出力される
+# 通常 → <__main__.Card object at 0x10c949310>
+# reprメソッド追加 → ♦︎-4
+
+# card = Card(2, 4)
+# print(card)
