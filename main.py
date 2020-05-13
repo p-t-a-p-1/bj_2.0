a8e2dd50cdd2254fcc5844b7814e01ae295908d5
p-t-a-p-1 <kb240225+1@gmail.com>
2020-05-13 09:28:24 +0900

    [fix] スコア表示箇所をヒアドキュメントに修正

--- main.py
+++ main.py
@@ -38,23 +38,27 @@ class Game:
         judge_win = ""
         if dealer.card_current_score < \
                 player.card_current_score <= 21:
+            # dealer < player <= 21の時、playerの勝利
             judge_win = "player win!"
             player.win_count += 1
         elif player.card_current_score <= 21 \
                 < dealer.card_current_score:
+            # player が21以下、dealerがバーストはplayerの勝利
             judge_win = "player win!"
             player.win_count += 1
         elif player.card_current_score == dealer.card_current_score \
                 and player.card_current_score <= 21:
+            # どちらもバーストせず、同じ数字の場合は引き分け
             judge_win = "---draw---"
         else:
+            # それ以外の場合は全部playerの負け
             judge_win = "dealer win!"
             dealer.win_count += 1
         print(f"\n/***********/\n/{judge_win}/\n/***********/")
 
     def main(self):
         """
-        ゲームスタート
+        ブラックジャックのメインゲーム関数
         """
 
         # 山札セット（セット数を決める）
@@ -62,29 +66,40 @@ class Game:
         # 最初は２枚ずつドロー
         self.player.draw_card(deck, 2)
         self.dealer.draw_card(deck, 2)
+        # player初期スコア計算
+        self.player.calc_current_score(self.player.hands)
 
         # 初期ドロー時のスコア表示（dealer側の1枚は伏せる）
-        print(f"dealer's hands : [{self.dealer.hands[0]}, *-*]")
-        print()
-        print(f"player's hands : {self.player.hands}")
-        self.player.calc_current_score(self.player.hands)
-        print(f"players's total_score : {self.player.card_current_score}")
+        print("\n--Game Start--\n")
+
+        first_msg = f"""\
+dealer's hands : [{self.dealer.hands[0]}, *-*]
+player's hands : {self.player.hands}
+
+players's total_score : {self.player.card_current_score}\
+        """
+        print(f"{first_msg}")
 
         # playerに hit or stand 決めさせる（stand で player のターンが終了）
         self.player.keep_drawing_card(deck)
 
-        print("\n--result--")
+        print("\n--Result--\n")
 
-        # dealerの手札オープン
-        print(f"dealer's hands : {self.dealer.hands}")
+        # dealerスコア計算
         self.dealer.calc_current_score(self.dealer.hands)
-        print(f"dealer's total_score : {self.dealer.card_current_score}")
+        dealer_msg = f"""\
+dealer's hands : {self.dealer.hands}
+dealer's total_score : {self.dealer.card_current_score}
+        """
+        print(f"{dealer_msg}")
 
         # dealerの手札の合計が17になるまで引く
         self.dealer.keep_drawing_card(deck)
 
         # 勝敗判定
         self.judge_winner(self.dealer, self.player)
+
+        print("\n--Game End--\n")
 
 
 if __name__ == '__main__':
