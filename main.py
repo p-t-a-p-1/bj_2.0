e51d0396cb62c58393c910e43c8ffdad24ca21bc
p-t-a-p-1 <kb240225+1@gmail.com>
2020-05-15 09:26:26 +0900

    #2 [update] 複数ゲームできるように改修、スコア計算不具合修正

--- main.py
+++ main.py
@@ -23,16 +23,26 @@ class Game:
         self.player = role.Player()
         self.dealer = role.Dealer()
 
-    def compare_score(self, score_list):
+    def get_nearest_score(self, score_list):
+        """
+        メインスコアとサブスコアから21以下で21にもっとも近い数字を返す
+        どちらも21超えてる場合は0を返す
+
+        Parameters
+        ----------
+        score_list : list
+            メインスコアとサブスコアのリスト
+        """
+        main_score = 0
         for score in score_list:
-            main_score = 100
             if score > 21:
+                # 21より大きい数字はバースト
                 continue
-            if main_score > score:
+            elif main_score < score:
                 main_score = score
         return main_score
 
-    def judge_winner(self, dealer, player):
+    def judge_winner(self, player, dealer):
         """
         勝敗判定
 
@@ -44,17 +54,22 @@ class Game:
             子
         """
 
-        # dealer, player の各スコアで21以下の21に近い方を取得し比較
-        dealer_score_list = [
-            dealer.card_current_score,
-            dealer.card_current_score_sub]
-        dealer_score = self.compare_score(dealer_score_list)
+        # player, dealer の各スコアで21以下の21に近い方を取得し比較
         player_score_list = [
             player.card_current_score,
             player.card_current_score_sub]
-        player_score = self.compare_score(player_score_list)
+        player_score = self.get_nearest_score(player_score_list)
+
+        dealer_score_list = [
+            dealer.card_current_score,
+            dealer.card_current_score_sub]
+        dealer_score = self.get_nearest_score(dealer_score_list)
 
         judge_win = ""
+        # どちらもバーストはdraw
+        if player_score == 0 and dealer_score == 0:
+            judge_win = "---draw---"
+
         if dealer_score < \
                 player_score <= 21:
             # dealer < player <= 21の時、playerの勝利
@@ -73,7 +88,36 @@ class Game:
             # それ以外の場合は全部playerの負け
             judge_win = "dealer win!"
             dealer.win_count += 1
+        # コンソール表示
         print(f"\n/***********/\n/{judge_win}/\n/***********/")
+
+    def display_final_result(
+            self,
+            player_win_count,
+            dealer_win_count,
+            total_count):
+        """
+        勝敗判定
+
+        Parameters
+        ----------
+        player_win_count : int
+            playerの勝利回数
+        dealer_win_count : int
+            dealerの勝利回数
+        total_count : int
+            総ゲーム数
+        """
+        # 総ゲーム数からplayerとdealerの勝利数引いてドローの回数計算
+        draw_count = total_count - player_win_count - dealer_win_count
+        return f"""\
+*-*-*-*-*-*-*-*
+total：{total_count}
+win：{player_win_count}
+lose：{dealer_win_count}
+draw：{draw_count}
+*-*-*-*-*-*-*-*\
+"""
 
     def main(self):
         """
@@ -82,46 +126,76 @@ class Game:
 
         # 山札セット（セット数を決める）
         deck = stock.Deck()
-        # 最初は２枚ずつドロー
-        self.player.draw_card(deck, 2)
-        self.dealer.draw_card(deck, 2)
-        # player初期スコア計算
-        self.player.calc_current_score(self.player.hands)
-        player_sub_score = ""
-        if self.player.draw_A_flg is True:
-            player_sub_score = f", {self.player.card_current_score_sub}"
 
-        # 初期ドロー時のスコア表示（dealer側の1枚は伏せる）
-        print("\n--Game Start--\n")
+        total_count = 0
+        game_flg = True
+        # 残りカードが5枚以上の場合
+        while game_flg is True and len(deck.cards) > 5:
 
-        first_msg = f"""\
+            self.player.hands = []
+            self.dealer.hands = []
+
+            # ゲーム数+1
+            total_count += 1
+
+            # 最初は２枚ずつドロー
+            self.player.draw_card(deck, 2)
+            self.dealer.draw_card(deck, 2)
+            # player初期スコア計算
+            self.player.calc_current_score(self.player.hands)
+            print(self.player.draw_A_flg)
+            print(self.dealer.draw_A_flg)
+
+            player_sub_score = ""
+            if self.player.draw_A_flg is True:
+                player_sub_score = f", {self.player.card_current_score_sub}"
+
+            # 初期ドロー時のスコア表示（dealer側の1枚は伏せる）
+            print("\n--Game Start--\n")
+
+            first_msg = f"""\
 dealer's hands : [{self.dealer.hands[0]}, *-*]
 player's hands : {self.player.hands}
 
 players's total_score : {self.player.card_current_score}{player_sub_score}\
-        """
-        print(f"{first_msg}")
+            """
+            print(f"{first_msg}")
 
-        # playerに hit or stand 決めさせる（stand で player のターンが終了）
-        self.player.keep_drawing_card(deck)
+            # playerに hit or stand 決めさせる（stand で player のターンが終了）
+            self.player.keep_drawing_card(deck)
 
-        print("\n--Result--\n")
+            print("\n--Result--\n")
 
-        # dealerスコア計算
-        self.dealer.calc_current_score(self.dealer.hands)
-        dealer_msg = f"""\
+            # dealerスコア計算
+            self.dealer.calc_current_score(self.dealer.hands)
+            dealer_sub_score = ""
+            # A引いてる場合はサブスコアも表示
+            if self.dealer.draw_A_flg is True:
+                dealer_sub_score = f", {self.dealer.card_current_score_sub}"
+
+            dealer_msg = f"""\
 dealer's hands : {self.dealer.hands}
-dealer's total_score : {self.dealer.card_current_score}
-        """
-        print(f"{dealer_msg}")
+dealer's total_score : {self.dealer.card_current_score}{dealer_sub_score}\
+            """
+            print(f"{dealer_msg}")
 
-        # dealerの手札の合計が17になるまで引く
-        self.dealer.keep_drawing_card(deck)
+            # dealerの手札の合計が17になるまで引く
+            self.dealer.keep_drawing_card(deck)
 
-        # 勝敗判定
-        self.judge_winner(self.dealer, self.player)
+            # 勝敗判定
+            self.judge_winner(self.player, self.dealer)
 
-        print("\n--Game End--\n")
+            print("\n--Game End--\n")
+
+            # ゲームリスタート
+            restart_msg = "Qでゲーム終了、それ以外でゲームスタート："
+            start_res = input(restart_msg)
+            if start_res == 'Q':
+                game_flg = False
+
+        final_score_str = self.display_final_result(
+            self.player.win_count, self.dealer.win_count, total_count)
+        print(final_score_str)
 
 
 if __name__ == '__main__':
